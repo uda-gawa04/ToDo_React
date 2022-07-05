56680b30507fe0e00e709aae6f99ad4fbe610d97
uda-gawa04 <108511181+uda-gawa04@users.noreply.github.com>
2022-07-05 12:02:29 +0900

    タスクの追加機能を付ける

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,32 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  // 入力した値のステート化
+  const [todoText, setTodoText] = useState("");
   // 未完了のリストを格納する変数
   const [incompTodos, setIncompTodos] = useState(["aa", "bb"]);
   // 完了のリストを格納する変数
   const [compTodos, setCompTodos] = useState(["cc"]);
 
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  // 入力した文字を未完了のリストに追加する
+  const onClickAdd = () => {
+    // 空文字の時は追加できないようにする
+    if (todoText === "") return;
+    // 配列にテキストを設定するだけでステートが変更　→　ステート変更により再レンダリング
+    const newTodos = [...incompTodos, todoText];
+    setIncompTodos(newTodos);
+    setTodoText("");
+  };
   return (
     <>
       <div className="input-area">
-        <input placeholder="Enter TODO" />
-        <button>add</button>
+        <input
+          placeholder="Enter TODO"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>add</button>
       </div>
       <div className="incomp-area">
         <p className="title">incomplete</p>
