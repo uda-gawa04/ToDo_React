867d057f8e30af835a106a46dc223bda9a8221db
uda-gawa04 <108511181+uda-gawa04@users.noreply.github.com>
2022-07-05 10:46:17 +0900

    JSXで構造を作る

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,27 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>add</button>
+      </div>
+      <div>
+        <p>未完了</p>
+        <ul>
+          <li></li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+      </div>
+      <div>
+        <p>完了</p>
+        <ul>
+          <li></li>
+          <button>戻す</button>
+        </ul>
+      </div>
+    </>
+  );
 };
