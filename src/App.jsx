dad0ad6e58859bbd5623bbd2203dfd7deea6bae7
uda-gawa04 <108511181+uda-gawa04@users.noreply.github.com>
2022-07-05 13:58:24 +0900

    完成

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ export const App = () => {
   // 入力した値のステート化
   const [todoText, setTodoText] = useState("");
   // 未完了のリストを格納する変数
-  const [incompTodos, setIncompTodos] = useState(["aa", "bb"]);
+  const [incompTodos, setIncompTodos] = useState([]);
   // 完了のリストを格納する変数
-  const [compTodos, setCompTodos] = useState(["cc"]);
+  const [compTodos, setCompTodos] = useState([]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
   // 入力した文字を未完了のリストに追加する
@@ -20,11 +20,42 @@ export const App = () => {
     setTodoText("");
   };
 
+  // 削除ボタンに対する設定
   const onClickDelete = (index) => {
     const newTodos = [...incompTodos];
+    // 配列から削除する
     newTodos.splice(index, 1);
     setIncompTodos(newTodos);
   };
+
+  const onClickDelete2 = (index) => {
+    const newTodos1 = [...compTodos];
+    newTodos1.splice(index, 1);
+    setCompTodos(newTodos1);
+  };
+
+  // 完了ボタンの設定
+  const onClickComp = (index) => {
+    const newIncompTodos = [...incompTodos];
+    newIncompTodos.splice(index, 1);
+
+    const newConpTodos = [...compTodos, incompTodos[index]];
+
+    setIncompTodos(newIncompTodos);
+    setCompTodos(newConpTodos);
+  };
+
+  // 戻るボタンの設定
+  const onClickBack = (index) => {
+    // 完了エリアから削除
+    const newCompTodos = [...compTodos];
+    newCompTodos.splice(index, 1);
+    // 未完了に追加
+    const newIncompTodos = [...incompTodos, compTodos[index]];
+    setCompTodos(newCompTodos);
+    setIncompTodos(newIncompTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -44,7 +75,7 @@ export const App = () => {
               // key：レンダリングされている場合、何個目のやつなのかをしっかり把握するため
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>complete</button>
+                <button onClick={() => onClickComp(index)}>complete</button>
                 <button onClick={() => onClickDelete(index)}>delete</button>
               </div>
             );
@@ -54,11 +85,12 @@ export const App = () => {
       <div className="comp-area">
         <p className="title">complete</p>
         <ul>
-          {compTodos.map((todo) => {
+          {compTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
-                <li></li>
-                <button>return</button>
+                <li>{todo}</li>
+                <button onClick={() => onClickBack(index)}>return</button>
+                <button onClick={() => onClickDelete2(index)}>delete</button>
               </div>
             );
           })}
