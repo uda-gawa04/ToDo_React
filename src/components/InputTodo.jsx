40b28baf257402a55699817749bacbddbd40def5
uda-gawa04 <108511181+uda-gawa04@users.noreply.github.com>
2022-07-05 14:59:09 +0900

    CSS-in-JS

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#799959",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  bordeRadius: "8px"
+};
+
 export const InputTodo = (props) => {
   // 分割代入で、propsの中に入れて渡されたものを使いやすくする
   const { todoText, onChange, onClick } = props;
 
   return (
-    <div className="input-area">
+    <div style={style}>
       <input placeholder="MY TODO" value={todoText} onChange={onChange} />
       <button onClick={onClick}>add</button>
     </div>
